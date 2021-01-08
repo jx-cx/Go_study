ef713acb968d452e705c549d0c0f216e7135f048
杜鸿名 <807196611@qq.com>
2021-01-08 17:42:33 +0800

    20210108学习

---
+++ bool/bool.go
@@ -0,0 +1,24 @@
+package main
+
+/*
+Go语言中以bool类型进行声明布尔型数据，布尔型数据只有true（真）和false（假）两个值。
+
+注意：
+
+布尔类型变量的默认值为false。
+Go 语言中不允许将整型强制转换为布尔型.
+布尔型无法参与数值运算，也无法与其他类型进行转换。
+
+**/
+
+import "fmt"
+
+func main() {
+	var a bool
+	b := true
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T value:%v\n", b, b)
+
+	
+
+}
--- float/float.go
+++ float/float.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 Go语言支持两种浮点型数：float32和float64。
 这两种浮点型数据格式遵循IEEE 754标准： float32 的浮点数的最大范围约为 3.4e38，可以使用常量定义：math.MaxFloat32。
@@ -7,5 +9,18 @@ Go语言支持两种浮点型数：float32和float64。
 
 **/
 func main() {
+	a := 1.234
+	fmt.Printf("%T\n", a) // Go语言中的小数默认都是float64类型
+	b := float32(1.234)   //显示成名float32类型
+	fmt.Printf("%T\n", b)
+	// b=a   //float64与float32类型不能直接赋值
+
+	var c1 complex64
+	c1 = 1 + 2i
+	var c2 complex128
+	c2 = 2 + 3i
+	fmt.Println(c1)
+	fmt.Println(c2)
+	//复数有实部和虚部，complex64的实部和虚部为32位，complex128的实部和虚部为64位。
 
 }
