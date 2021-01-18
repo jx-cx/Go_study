bbb34bee87c8b8eca1a569a5247d3cebaea6b805
wy-1996 <wy791251227@163.com>
2021-01-18 23:11:59 +0800

    20210118学习

---
+++ map/map.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"time"
+)
+
+/*
+map是一种无序的基于key-value的数据结构，Go语言中的map是引用类型，必须初始化才能使用。
+Go语言中 map的定义语法如下：
+
+map[KeyType]ValueType
+其中，
+
+KeyType:表示键的类型。
+ValueType:表示键对应的值的类型。
+map类型的变量默认初始值为nil，需要使用make()函数来分配内存。语法为：
+
+make(map[KeyType]ValueType, [cap])
+**/
+func main() {
+	scoreMap := make(map[string]int, 8)
+	scoreMap["张三"] = 90
+	scoreMap["小明"] = 100
+	fmt.Println(scoreMap)
+	fmt.Println(scoreMap["小明"])
+	fmt.Printf("type of a:%T\n", scoreMap)
+	// 如果key存在ok为true,v为对应的值；不存在ok为false,v为值类型的零值
+	v, ok := scoreMap["张三"]
+	if ok {
+		fmt.Println(v)
+	} else {
+		fmt.Println("查无此人")
+	}
+
+	// 遍历map
+	for k := range scoreMap {
+		fmt.Println(k)
+	}
+
+	for _, k := range scoreMap {
+		fmt.Println(k)
+	}
+	//使用delete()内建函数从map中删除一组键值对
+	delete(scoreMap, "小明") //将小明:100从map中删除
+	fmt.Println(scoreMap)
+
+	rand.Seed(time.Now().UnixNano()) //初始化随机数种子
+
+	var a1 = make(map[string]int, 200)
+
+	for i := 0; i < 100; i++ {
+		key := fmt.Sprintf("stu%02d", i) //生成stu开头的字符串
+		value := rand.Intn(100)          //生成0~99的随机整数
+		a1[key] = value
+	}
+	// fmt.Println(a1)
+	//取出map中的所有key存入切片keys
+	var keys = make([]string, 0, 200)
+	for key := range a1 {
+		keys = append(keys, key)
+	}
+	//对切片进行排序
+	sort.Strings(keys)
+	//按照排序后的key遍历map
+	for _, key := range keys {
+		fmt.Println(key, a1[key])
+	}
+	//元素为map类型的切片
+	var mapSlice = make([]map[string]string, 3)
+	for index, value := range mapSlice {
+		fmt.Printf("index:%d value:%v\n", index, value)
+	}
+	fmt.Println("after init")
+	// 对切片中的map元素进行初始化
+	mapSlice[0] = make(map[string]string, 10)
+	mapSlice[0]["name"] = "小王子"
+	mapSlice[0]["password"] = "123456"
+	mapSlice[0]["address"] = "沙河"
+	for index, value := range mapSlice {
+		fmt.Printf("index:%d value:%v\n", index, value)
+	}
+	//值为切片类型的map
+	var sliceMap = make(map[string][]string, 3)
+	fmt.Println(sliceMap)
+	fmt.Println("after init")
+	key := "中国"
+	value, ok := sliceMap[key]
+	if !ok {
+		value = make([]string, 0, 2)
+	}
+	value = append(value, "北京", "上海")
+	sliceMap[key] = value
+	fmt.Println(sliceMap)
+
+	type Map map[string][]int
+	m := make(Map)
+	s := []int{1, 2}
+	s = append(s, 3)
+	fmt.Printf("%+v\n", s)
+	m["q1mi"] = s
+	s = append(s[:1], s[2:]...)
+	fmt.Printf("%+v\n", s)
+	fmt.Printf("%+v\n", m["q1mi"])
+}
--- slice_copy/slice_copy.go
+++ slice_copy/slice_copy.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 /*
 由于切片是引用类型，所以a和b其实都指向了同一块内存地址。修改b的同时a的值也会发生变化。
@@ -22,6 +25,19 @@ func main() {
 		**/
 	//删除索引为1的元素
 	a1 = append(a1[:1], a1[2:]...) //将 a1 第2个索引到最后的元素追加到 a1 从0至1的索引后面  1 3
+	// append修改了底层数组，将a1 变成了 1 5 5
 	fmt.Println(a1)
 	fmt.Println(cap(a1))
+
+	var a = make([]string, 5, 10)
+	for i := 0; i < 10; i++ {
+		a = append(a, fmt.Sprintf("%v", i))
+	}
+	fmt.Println(a)
+	fmt.Println()
+	fmt.Println(a[5])
+
+	var a5 = [...]int{3, 7, 8, 9, 1}
+	sort.Ints(a5[:]) //sort.Ints 对切片进行排序
+	fmt.Println(a5)
 }
